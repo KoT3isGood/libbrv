7d77b5bc34b962cd6189b756db78bcbaf1e0c609
KoT3isGood <bratelllo@gmail.com>
2025-01-12 13:54:41 +0200

    fixed build system

--- build.zig
+++ build.zig
@@ -28,6 +28,24 @@ pub fn build(b: *std.Build) void {
     lib_artifact.implib_dir = lib_artifact.dest_dir;
     b.getInstallStep().dependOn(&lib_artifact.step);
 
+    const slib = b.addStaticLibrary(.{
+        .name = "brv_static",
+        .target = target,
+        .optimize = optimize,
+    });
+    slib.addCSourceFiles(.{
+        .files = &.{
+            //
+            "source/parser.c",
+            "source/analyzer.c",
+            "source/builder.c",
+        },
+    });
+    slib.linkLibC();
+    const slib_artifact = b.addInstallArtifact(slib, .{});
+    slib_artifact.dest_dir = .{ .custom = "../lib/" };
+    b.getInstallStep().dependOn(&slib_artifact.step);
+
     // build examples
     const parsing = b.addExecutable(.{ .name = "parsing", .target = target, .optimize = optimize });
     parsing.addCSourceFile(.{ .file = b.path("examples/parsing/main.c") });
@@ -35,8 +53,9 @@ pub fn build(b: *std.Build) void {
         //parsing.addCSourceFile(.{ .file = b.path("") });
     }
     parsing.linkLibC();
-    parsing.linkLibrary(lib);
+    parsing.linkLibrary(slib);
     const parsing_artifact = b.addInstallArtifact(parsing, .{});
     parsing_artifact.dest_dir = .{ .custom = "../bin/parsing" };
+    parsing_artifact.pdb_dir = .{ .custom = "../bin/parsing" };
     b.getInstallStep().dependOn(&parsing_artifact.step);
 }
